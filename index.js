92347c6f653be66194fd32143661719ca2e9fb0e
Alexander Seitz <alexseitz248@gmail.com>
2022-07-24 12:04:01 +0200

    fix: correct string format

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const generateImage = require("./generateImage");
 
 const client = new Discord.Client({ 
     intents: [
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
+        'GUILDS',
+        'GUILD_MESSAGES',
+        'GUILD_MEMBERS',
     ]
 });
 
-client.on("ready", () => {
+client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("こんにちは");
+client.on('messageCreate', (message) => {
+    if (message.content === 'hi'){
+        message.reply('こんにちは');
     }
 });
 
-const welcomeChannelId = "954728690619662427";
+const welcomeChannelId = '954728690619662427';
 
-client.on("guildMemberAdd", async (member) => {
+client.on('guildMemberAdd', async (member) => {
     const img = await generateImage(member);
     member.guild.channels.cache.get(welcomeChannelId).send({
         content: `<@${member.id}> Welcome to the server!`,
