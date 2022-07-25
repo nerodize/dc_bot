b337532a9307646ce4adbf64265132b0e515d3d2
Alexander Seitz <alexseitz248@gmail.com>
2022-07-25 13:29:18 +0200

    fix: changed string format to standard

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require("discord.js");
-require("dotenv").config();
+const Discord = require('discord.js');
+require('dotenv').config();
 
-const generateImage = require("./generateImage");
+const generateImage = require('./generateImage');
 
 const client = new Discord.Client({ 
     intents: [
