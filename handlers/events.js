14f0aa552e0b17947aa6627ce33402552a55b5fb
Alexander Seitz <alexseitz248@gmail.com>
2022-09-08 20:27:35 +0200

    feat: added event logic

--- handlers/events.js
+++ handlers/events.js
@@ -9,8 +9,36 @@ module.exports = (bot, reload) => {
         console.log('No Events to load');
     }
 
-    events.forEach((f, i) => {
-        const event = require(`../events/${f}`);
+    events.forEach((file, index) => {
+        if (reload)
+            delete require.cache[require.resolve(`../events/${file}`)];
+        const event = require(`../events/${file}`);
         client.events.set(event.name, event);
+
+        if (!reload)
+            console.log(`${index + 1}. ${file} loaded`);
     });
+}
+
+
+function triggerEventHandler(bot, event, ...args) {
+    const {client} = bot;
+
+    try {
+        if (client.events.has(event))
+            client.events.get(event).run(bot, ...args);
+        else 
+            throw new Error(`Event ${event} dows not exist!`);
+    }
+    catch(err) {
+        console.error(err);
+    }
+}
+
+function initEvents(bot) {
+    const {client} = bot;
+
+    client.on('ready', () => {
+        triggerEventHandler(bot, 'ready');
+    })
 }
